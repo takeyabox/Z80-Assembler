f947fe47a142b3db996017e7af1a5f1f01a194b1
soma <takeyabox84@gmail.com>
2025-09-29 11:59:09 +0900

    Add files via upload
    I remade LEDmat and asm.bat

--- LEDmat.asm
+++ LEDmat.asm
@@ -1,67 +1,47 @@
 IOsetup:
-    LD A, 0x0F
+    LD A, 0x4F
     OUT (02), A
     LD A, 0x0F
     OUT (03), A
     RET
 
 display:
-; Display the contents of memory locations F0 to F7 on the LED matrix
-; port 00 selects the row, port 01 sets the column data
-    LD A, 0x01
-    OUT (00), A
     LD A, (0x00F0)
-    OUT (01), A
-    LD A, 0x02
-    OUT (00), A
+    ADD A, 0x10
+    OUT (0x01), A
     LD A, (0x00F1)
-    OUT (01), A
-    LD A, 0x04
-    OUT (00), A
+    ADD A, 0x20
+    OUT (0x01), A
     LD A, (0x00F2)
-    OUT (01), A
-    LD A, 0x08
-    OUT (00), A
+    ADD A, 0x40
+    OUT (0x01), A
     LD A, (0x00F3)
-    OUT (01), A
-    LD A, 0x10
-    OUT (00), A
-    LD A, (0x00F4)
-    OUT (01), A
-    LD A, 0x20
-    OUT (00), A
-    LD A, (0x00F5)
-    OUT (01), A
-    LD A, 0x40
-    OUT (00), A
-    LD A, (0x00F6)
-    OUT (01), A
-    LD A, 0x80
-    OUT (00), A
-    LD A, (0x00F7)
-    OUT (01), A
+    ADD A, 0x80
+    OUT (0x01), A
     RET
+
 START:
     CALL IOsetup
     LD A, 0x00
+    LD B, 0x00
 
 LOOP:
-LD (0x00F0), A
-INC A
-LD (0x00F1), A
-INC A
-LD (0x00F2), A
-INC A
-LD (0x00F3), A
-INC A
-LD (0x00F4), A
-INC A
-LD (0x00F5), A
-INC A
-LD (0x00F6), A
-INC A
-LD (0x00F7), A
-CALL display
-JP LOOP
+    LD (0x00F0), A
+    INC A
+    LD (0x00F1), A
+    INC A
+    LD (0x00F2), A
+    INC A
+    LD (0x00F3), A
+    CALL display
+    INC A
+    INC B
+    LD C, A
+    LD A, B
+    SUB A, 0x04
+    LD A, C
+    JP NZ, LOOP
+    JP START
+
 ; End of program
 
--- Z80Assembler.bat
+++ Z80Assembler.bat
@@ -1,2 +1,2 @@
 echo off
-cmd /k C:\Users\C22195\Downloads\Z80ƒAƒZƒ“ƒuƒ‰\asez80.exe -l -s -o %1
+cmd /k asez80 -l -s -o %1
